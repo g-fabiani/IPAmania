38945cfcde0164e52481cd3098d10660066408a9
Giulia Fabiani <giulia.fabiani01@gmail.com>
2022-12-19 01:12:47 +0100

    Modified layout

--- phonetic_alphabet.js
+++ phonetic_alphabet.js
@@ -1,7 +1,7 @@
 "use strict"
 
 // Costanti
-const MATCHES = 5;		// numero di coppie per round
+const MATCHES = 6;		// numero di coppie per round
 const DELAY = 500;
 
 
@@ -65,7 +65,7 @@ function checkMatch() {
 		matched++;
 		console.log(matched);
 		if (matched == MATCHES) {
-			console.log("hai vinto");
+			addText(messageNode, "Vittoria! Premi sul pulsante nuova partita per giocare ancora");
 			clearInterval(timer);
 		}
 
@@ -105,6 +105,9 @@ function handlerNewGame() {
 		matched = 0;
 		penalities = 0;
 		time = 0;
+
+		// Toglie il messaggio
+		addText(messageNode, "");
 
 		// Azzera le associazioni tra id delle tessere e chiavi dell'array IPA_VOWELS
 		keySymbols = {};
@@ -220,6 +223,7 @@ var newGameNode;
 var cardGridNode;
 var scoreNode;
 var timerNode;
+var messageNode;
 
 var symbolNodes = [];
 var descriptionNodes = [];
@@ -241,7 +245,8 @@ function handlerLoad() {
 		newGameNode = document.getElementById("new_game");
 		cardGridNode = document.getElementById("card_grid");
 		scoreNode = document.getElementById("score");
-		timerNode = document.getElementById('timer')
+		timerNode = document.getElementById("timer")
+		messageNode = document.getElementById("message")
 
 		for (var i = 0; i < MATCHES; i++) {
 
@@ -310,15 +315,16 @@ const IPA_VOWELS = {
 	12 : {symbol: "\u0275", description: "Vocale centrale semichiusa arrotondata"},
 	13 : {symbol: "\u0264", description: "Vocale posteriore semichiusa non arrotondata"},
 	14 : {symbol: "\u006f", description: "Vocale posteriore semichiusa arrotondata"},
-	15 : {symbol: "\u025b", description: "Vocale anteriore semiaperta non arrotondata"},
-	16 : {symbol: "\u0153", description: "Vocale anteriore semiaperta arrotondata"},
-	17 : {symbol: "\u025c", description: "Vocale centrale semiaperta non arrotondata"},
-	18 : {symbol: "\u025e", description: "Vocale centrale semiaperta arrotondata"},
-	19 : {symbol: "\u0254", description: "Vocale posteriore semiaperta arrotondata"},
-	20 : {symbol: "\u00e6", description: "Vocale anteriore quasi aperta non arrotondata"},	
-	21 : {symbol: "\u0250", description: "Vocale centrale quasi aperta"},
-	22 : {symbol: "\u0061", description: "Vocale anteriore aperta non arrotondata"},
-	23 : {symbol: "\u0276", description: "Vocale anteriore aperta arrotondata"},
-	24 : {symbol: "\u0251", description: "Vocale posteriore aperta non arrotondata"},
-	25 : {symbol: "\u0252", description: "Vocale posteriore aperta arrotondata"},
+	15 : {symbol: "\u0259", description: "Vocale centrale media"},
+	16 : {symbol: "\u025b", description: "Vocale anteriore semiaperta non arrotondata"},
+	17 : {symbol: "\u0153", description: "Vocale anteriore semiaperta arrotondata"},
+	18 : {symbol: "\u025c", description: "Vocale centrale semiaperta non arrotondata"},
+	19 : {symbol: "\u025e", description: "Vocale centrale semiaperta arrotondata"},
+	20 : {symbol: "\u0254", description: "Vocale posteriore semiaperta arrotondata"},
+	21 : {symbol: "\u00e6", description: "Vocale anteriore quasi aperta non arrotondata"},	
+	22 : {symbol: "\u0250", description: "Vocale centrale quasi aperta"},
+	23 : {symbol: "\u0061", description: "Vocale anteriore aperta non arrotondata"},
+	24 : {symbol: "\u0276", description: "Vocale anteriore aperta arrotondata"},
+	25 : {symbol: "\u0251", description: "Vocale posteriore aperta non arrotondata"},
+	26 : {symbol: "\u0252", description: "Vocale posteriore aperta arrotondata"},
 }
